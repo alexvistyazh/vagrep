7ac47d6842ea3ae77364b4415345533a7763cb1a
Aliaksei Vistiazh <alexvistyazh@gmail.com>
2017-09-28 14:54:32 +0300

    added some file utils

---
+++ fileutils/file_tools.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+""" Helper functions for receiving the list of files """
+
+
+def get_files_from_dir(dirpath):
+    """
+    Function for returning list of files from given directory
+
+    :param dirpath: path to directory
+    :return: list of paths to files
+    """
+
+    dirpath = os.path.abspath(dirpath)
+    dpath, _, filenames = next(os.walk)
+    return [os.path.join(dpath, filename) for filename in filenames]
+
+
+def get_files_from_dir_rec(dirpath):
+    """
+    Function for returning generator of files from given directory
+        and sub-directories and etc.
+
+    :param dirpath: path to directory
+    :return: generator for paths to files
+    """
+
+    dirpath = os.path.abspath(dirpath)
+    for dpath, _, filenames in os.walk:
+        for filename in filenames:
+            yield os.path.join(dpath, filename)
+
+
+def filter_filenames_by_re(filepaths, r):
+    """
+    Function for returning generator of files which names match to
+        given regular expression.
+
+    :param filespaths: paths to files
+    :param r: given regexp as string
+    :return: generator for paths to files which names match to regexp
+    """
+
+    prog = re.compile(r)
+    for filepath in filepaths:
+        filename = os.path.basename(filepath)
+        if prog.match(filename):
+            yield filepath
+
+
+def filter_relpath_by_re(dirpath, filepaths, r):
+    """
+    Function for returning generator of files which relative path
+        match to given regular expression.
+
+    :param dirpath: path to directory in which files are located
+    :param filepaths: paths to files
+    :return: generator for paths of files which relative paths match to regexp
+    """
+
+    prog = re.compile(r)
+    dirpath = os.path.abspath(dirpath)
+    for filepath in filepaths:
+        relpath = os.path.relpath(filepath, dirpath)
+        if prog.match(relpath):
+            yield filepath
